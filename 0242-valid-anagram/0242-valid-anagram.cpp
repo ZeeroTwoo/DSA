6e3ce248170ba90ef1910d9f9e34934672597531
Kaushik Dugar <deerex1211@gmail.com>
2026-08-21 23:46:49 +0530

    Time: 0 ms (100%), Space:9.6 MB (92.54%)

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-        int count[26] = {0};
-        for (int i = 0; i < s.size(); i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
-        }
-        for (int x : count) {
-            if (x != 0) return false;
-        }
-        return true;
+       if(s.size()!=t.size()) return false;
+       int freq[26]={0};
+       for(int i=0;i<s.size();i++){
+        freq[s[i]-'a']++;
+        freq[t[i]-'a']--;
+       }
+       for(int x : freq){
+        if(x != 0) return false;
+       }
+       return true;
     }
 };
